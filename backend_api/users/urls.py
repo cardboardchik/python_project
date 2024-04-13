d6212e87d1f605aefb6ec9e99681f60918714448
Khen Artur <artyrzzo9@gmail.com>
2024-04-13 21:50:02 +0500

    add swagger

---
+++ backend_api/users/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'', views.UserViewSet, basename='user')
+
+
+
+
+urlpatterns = [
+    path('', include(router.urls))
+]
+
+
+
