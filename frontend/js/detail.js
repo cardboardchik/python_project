c2d8f9f89ea232a50852d1113238abfb8f5fc7e5
Khen Artur <artyrzzo9@gmail.com>
2024-05-01 23:44:39 +0500

    fix: frontend reviews

--- frontend/js/detail.js
+++ frontend/js/detail.js
@@ -41,4 +41,61 @@ $(function() {
             }
         }
     });
+    let data = {
+        "rating": 10,
+        "text": "text",
+        "item": item_id,
+    }
+    $("#review_button").click(function(){
+        data["rating"] = $("#rating_form").val();
+        data["text"] = $("#message").val();
+        if (data["rating"] == ""){
+            throw alert("input rating cannot be empty")
+        }
+        $.ajax({
+            url: "https://pythonproject-production-009d.up.railway.app/api/v1/store/reviews/",
+            type: 'POST',
+            headers: {'Authorization': 'Token bd3779a81b110537cd8cf136e8e1c549ca741afc'},
+            data: data,
+            dataType: 'json',
+            success: function(res) {
+                // alert(res)
+                $("#review_form").fadeOut()
+                $.ajax({
+                    url: `https://pythonproject-production-009d.up.railway.app/api/v1/store/item/${item_id}`,
+                    type: 'GET',
+                    dataType: 'json', // added data type
+                    success: function(res) {
+                        console.log(res)
+                        const reviews_view = document.querySelector("#reviews_view")
+            
+                        let name = res["name"]
+                        let reviews = res["reviews"]
+            
+                        $("#product_reviews_count_1").text(`(${reviews.length} Reviews)`)
+                        $("#product_reviews_count_2").text(`Reviews (${reviews.length})`)
+                        $("#product_reviews_count_3").text(`${reviews.length} review for ${name}`)
+            
+                        reviews_view.innerHTML += 
+                            `<div class="media mb-4">
+                                <img src="https://icons.veryicon.com/png/o/internet--web/prejudice/user-128.png" alt="Image" class="img-fluid mr-3 mt-1" style="width: 45px;">
+                                <div class="media-body">
+                                    <h6>${reviews.at(-1)["user"]}<small> - <i>${reviews.at(-1)["date"]}</i></small></h6>
+                                    <div class="text-primary mb-2">
+                                        <b>${reviews.at(-1)["rating"]}/10</b>
+                                    </div>
+                                    <p>${reviews.at(-1)["text"]}</p>
+                                </div>
+                            </div>`
+                    }  
+                });   
+            },
+
+            error: function(res){
+                alert(res.responseJSON["error"])
+            }
+        });
+    });
+    
+
 });
